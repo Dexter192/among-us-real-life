ee2a2646a19f00b72aeeccf923c0c90b144fbda6
Daniel Kaestner <DanielKaestner@outlook.de>
2026-01-01 20:54:03 +0100

    Wrap local storage in try catch to prevent invalid sessions

--- web/src/hooks/useAuthId.js
+++ web/src/hooks/useAuthId.js
@@ -2,14 +2,22 @@ import { useEffect, useState } from "react";
 
 export function useAuthId() {
   const [authId, setAuthId] = useState(() => {
-    return localStorage.getItem("authId") || "";
+    try {
+      return localStorage.getItem("authId") || "";
+    } catch {
+      return "";
+    }
   });
 
   useEffect(() => {
     if (!authId) {
       const newAuthId =
         Math.random().toString(36).slice(2) + Date.now().toString(36);
-      localStorage.setItem("authId", newAuthId);
+      try {
+        localStorage.setItem("authId", newAuthId);
+      } catch {
+        console.warn("Unable to access localStorage to set authId");
+      }
       setAuthId(newAuthId);
     }
   }, [authId]);
